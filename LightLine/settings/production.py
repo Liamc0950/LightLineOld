9bad1cdb7e86cdf48092c9b184eb11e7d56b0019
Liam Corley <liamc0950@gmail.com>
2020-04-22 19:29:28 -0400

    switch to production secret key

--- LightLine/settings/production.py
+++ LightLine/settings/production.py
@@ -1,7 +1,6 @@
 from .base import *
 
-#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-SECRET_KEY = '+^s!s50=3($xx(8+p&a12kff$*fz+wn!udi2)q)=q3p_#%0nmz'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 DEBUG = False
 
