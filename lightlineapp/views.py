48b6a629acfb75e193f931166a51c2175da364d3
Liam Corley <liamc0950@gmail.com>
2020-05-04 15:26:37 -0400

    color upload

--- lightlineapp/views.py
+++ lightlineapp/views.py
@@ -164,6 +164,23 @@ def followspots(request):
         'projectColorFlags' : projectColorFlags,
     }
 
+    csv = open('lightlineapp/roscolux.csv', 'r')  
+    for line in csv:
+        print(line)
+        line =  line.split(',')
+        print("Hello" + str(len(line[0])) + "there")
+        if len(line[0]) == 1:
+            pass
+        else: 
+            color = Color()  
+            color.colorName = line[1]
+            color.colorCode = line[0]  
+            color.colorHex = line[2]
+            color.save()  
+
+    csv.close() 
+
+
     return HttpResponse(template.render(context, request))
 
 #Followspot Modal Views
